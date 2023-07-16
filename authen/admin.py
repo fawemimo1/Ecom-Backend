9eb08a7242c5bcda0b722aba294c5198a970c4f0
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-16 08:08:51 +0100

    aaa

--- authen/admin.py
+++ authen/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import User
+
+# Register your models here.
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.site_header = 'NCPLISO'
+# admin.site.index_title = 'NCPLISO'
+# admin.site.site_title = 'NCPLISO'
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username','first_name','email',]
+
+
+admin.site.register(User, CustomUserAdmin)
+
