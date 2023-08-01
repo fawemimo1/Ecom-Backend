93aa9c28c331c2e50fd596a1c6d8e36c74838c54
parkson-tano <tanoparksonsilencer@gmail.com>
2023-08-01 18:49:49 +0000

    formpa

--- product/views.py
+++ product/views.py
@@ -23,26 +23,26 @@ class ImageFetchAPIView(viewsets.ModelViewSet):
         product_id = self.request.query_params.get('product_id')
         queryset = ProductImage.objects.filter(product=product_id)
         return queryset
-    
+
 class CategoryProductFetchAPIView(viewsets.ModelViewSet):
     serializer_class = ProductDetailSerializer
     def get_queryset(self):
         category_id = self.request.query_params.get('category_id')
         queryset = Product.objects.filter(category=category_id)
         return queryset
-    
+
 class HomeBannerImageAPIView(viewsets.ModelViewSet):
     serializer_class = HomeBannerImageSerializer
     def get_queryset(self):
         queryset = HomeBannerImage.objects.all()
         return queryset
-    
+
 class SizeAPIView(viewsets.ModelViewSet):
     serializer_class = SizeSerializer
     def get_queryset(self):
         queryset = Size.objects.all()
         return queryset
-    
+
 
 class ColorAPIView(viewsets.ModelViewSet):
     serializer_class = ColorSerializer
