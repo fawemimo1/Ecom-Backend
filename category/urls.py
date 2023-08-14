77cee190b8ea9fb22a6f557c16dd45a9d5255f43
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-08-14 11:52:23 +0100

    subcat api

--- category/urls.py
+++ category/urls.py
@@ -6,8 +6,10 @@ router = routers.DefaultRouter()
 router.register('category', CategoryViewAPI, basename='category_api')
 router.register('subcategory', SubCategoryViewAPI, basename='subcategory_api')
 router.register('subcategory-detail', SubCategoryDetailViewAPI, basename='subcategory_api')
+router.register('subcategory-category-fetch', SubCategoryCategoryFetchAPIView, basename='subcategory_api')
 router.register('brand', BrandViewAPI, basename='brand_api')
 router.register('brand-detail', BrandDetailViewAPI, basename='brand_api')
+router.register('brand-subcategory-fetch', BrandSubCategoryFetchAPIView, basename='brand_api')
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- category/views.py
+++ category/views.py
@@ -15,6 +15,20 @@ class SubCategoryDetailViewAPI(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategoryDetailSerializer
 
+class SubCategoryCategoryFetchAPIView(viewsets.ModelViewSet):
+    serializer_class = SubCategoryDetailSerializer
+    def get_queryset(self):
+        cat_id = self.request.query_params.get('cat_id')
+        queryset = SubCategory.objects.filter(category =  cat_id)
+        return queryset
+    
+class BrandSubCategoryFetchAPIView(viewsets.ModelViewSet):
+    serializer_class = BrandDetailSerializer
+    def get_queryset(self):
+        subcat_id = self.request.query_params.get('subcat_id')
+        queryset = Brand.objects.filter(subcategory =  subcat_id)
+        return queryset
+
 class BrandViewAPI(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
